aca538f93c4702d7e8989a05e3e1164fdf079d5a
anami823 <anami823@gmail.com>
2019-07-06 21:03:24 -0700

    Created 1st card flipped var and added to play arr

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,10 @@
-var cards =["queen", "queen", "king", "king"]
+var cards =["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0]; //First card flipped by player
+
+//Add cardOne flipped by plyer to cardsInPlay
+cardsInPlay.push(cardOne);
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+
