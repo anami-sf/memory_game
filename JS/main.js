180838f9e0373dcc905610d7422c4141a4444df4
anami823 <anami823@gmail.com>
2019-07-06 20:43:40 -0700

    Added cards array with kings and queens

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,4 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards =["queen", "queen", "king", "king"]
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
