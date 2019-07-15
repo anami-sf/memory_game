b45cca94621f7378755a6add54bdac54434a2cd9
anami823 <anami823@gmail.com>
2019-07-15 08:54:05 -0700

    Added id to each card object in the createBoard function. Updated flipCard to only check for match if the cards are not already facing up.

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,5 @@
 //Challange: add "reset" button and keep score
 
-var cardsInPlay = [];
 var cards =[
     {
         rank: "queen",
@@ -28,24 +27,25 @@ const createBoard = () => {
         var cardElement = document.createElement('img')
         cardElement.src = "images/back.png";
         cardElement.setAttribute('data-id', i);
+        cards[i].id = i
         board.appendChild(cardElement)
         cardElement.addEventListener('click', flipCard)
     }
 }
 
 //Task: remove flipped card from cards if it is a match
-
-var matchedCards = []
+var cardsInPlay = [];
+var matchedCards = [];
 
 const checkForMatch = () => {
 
     if (cardsInPlay.length === 2){
         if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
-            
-            matchedCards = matchedCards.concat(cardsInPlay)
             console.log("Cards: " + cards)            
             console.log("Cards in Play: " + cardsInPlay.length)
+            matchedCards = matchedCards.concat(cardsInPlay)
             console.log("Matched Cards: " + matchedCards) 
+            
             cardsInPlay = []
             console.log("You found a match!");
 
@@ -57,16 +57,19 @@ const checkForMatch = () => {
 }
 
 const flipCard = (event) => {
+
     //Should be able to use "this" instaead of event.target
     var cardId = event.target.getAttribute('data-id');
-    console.log("flipped Card: " + cardId)
+  
+    if (matchedCards.includes(cardId) != true) {
+        
+        event.target.src = cards[cardId].image
+        console.log("User flipped " + cards[cardId].rank);
+        cardsInPlay.push(cardId);
+    
+        checkForMatch()
+    }
 
-    event.target.src = cards[cardId].image
-
-    console.log("User flipped " + cards[cardId].rank);
-    cardsInPlay.push(cards[cardId]);
- 
-    checkForMatch()
 }
 
 createBoard()
