b38778433de7dfca52dbe440bf35624b8657c6fe
anami823 <anami823@gmail.com>
2019-07-14 22:44:52 -0700

    Updated flipCard function to remove cards from the cards array when they are in the array cardsInPlay

--- JS/main.js
+++ JS/main.js
@@ -21,26 +21,6 @@ var cards =[
     }  
 ];
 
-/* When a card is flipped we need to change the image src
-
-1. select element
-3. create function to update image src
-2. create event listener
-
-const flipCard = (cardId) => {
-
-
-    console.log("User flipped " + cards[cardId].rank);
-    
-    //Add card flipped by plyer to cardsInPlay
-    cardsInPlay.push(cards[cardId].rank);
-    console.log("Cards in play: " + cardsInPlay);
-    
-    checkForMatch()
-}
-
-*/
-
 var board = document.querySelector('#game-board');
 
 const createBoard = () => {
@@ -52,6 +32,8 @@ const createBoard = () => {
         cardElement.addEventListener('click', flipCard)
     }
 }
+
+//Task: remove flipped card from cards if it is a match
 
 const checkForMatch = () => {
 
@@ -74,9 +56,12 @@ const flipCard = (event) => {
     event.target.src = cards[cardId].image
 
     console.log("User flipped " + cards[cardId].rank);
-    //Add card flipped by plyer to cardsInPlay
-    cardsInPlay.push(cards[cardId]);
-    console.log("Cards in play: " + cardsInPlay);
+    //Pull the flipped card from cards and add it to to cardsInPlay
+    console.log("cardId: " + cardId)
+    const cardUp = cards.splice(0, 1)[0];
+    console.log("deleted:" + cardUp.rank)
+    cardsInPlay.push(cardUp);
+    console.log("Cards in play: " + cardsInPlay.length);
     checkForMatch()
 }
 
